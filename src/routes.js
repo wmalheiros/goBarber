74c971cf5a3e7360587f8cb1b5b6f30d5aa00d49
Wenderson Rodrigues Malheiros <wenderson.malheiros@gmail.com>
2019-04-24 15:24:56 -0300

    User model and migration

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
-  return res.send('hello word')
+  res.render('auth/signup')
 })
 
 module.exports = routes
