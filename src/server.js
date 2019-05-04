b0364dc5c9bb41eda5f270db5707c3944afbc623
Wenderson Rodrigues Malheiros <wenderson.malheiros@gmail.com>
2019-05-04 13:52:42 -0300

    User authentication

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const session = require('express-session')
+const LokiStore = require('connect-loki')(session)
 const nunjucks = require('nunjucks')
 const path = require('path')
+const flash = require('connect-flash')
 
 class App {
   constructor () {
@@ -13,6 +16,17 @@ class App {
   }
   middlewares () {
     this.express.use(express.urlencoded({ extended: false }))
+    this.express.use(flash())
+    this.express.use(
+      session({
+        store: new LokiStore({
+          path: path.resolve(__dirname, '..', 'tmp', 'sessions.db')
+        }),
+        secret: 'MyAppSecret',
+        resave: false,
+        saveUninitialized: true
+      })
+    )
   }
 
   views () {
